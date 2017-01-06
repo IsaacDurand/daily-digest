24935afdfe0f167cad91f7b84ec1ec4b1257280f
Isaac Durand <isaac.durand@gmail.com>
2017-01-05 20:18:10 -0800

    Use RequestBin to confirm that SMS was successfully delivered

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ var app = express();
 var client = twilio(secrets.accountSid, secrets.authToken);
 var options = {root: __dirname};
 
+// console.log(client); // has a lot of stuff :)
+// console.log(client.messages); // has get, list, post, and create methods
+// I think this basically lets us interact with the Messages list resource.
+// We can easily get a message if we have its SID - but what if we don't?
+
 app.get('/', function(req, res) {
   res.sendFile('index.html', options);
 });
@@ -22,18 +27,30 @@ app.post('/', function(req, res) {
   var message = req.body.message;
 
   if (message) {
-    // TODO: Review Twilio docs to understand what's going on here
+    // Would it be better to use client.sendMessage?
+    // See http://twilio.github.io/twilio-node/#quickstart
     client.messages.create({
       to: secrets.myMobileNumber,
       from: secrets.myTwilioNumber,
       body: message,
+      // Is there any way I can use the statusCallback option before actually
+      // deploying this?
+      // With the setup below, I can see in Request Bin that my SMS was
+      // successfully delivered.
+      statusCallback: secrets.requestBinUrl
     }, function(err, message) {
-      console.log(message.sid);
+      if (!err) {
+        // The message object contains a lot of information about the SMS I just
+        // created. However, it doesn't indicate whether the message was
+        // actually sent - only that it was queued.
+      }
     });
   }
 
-  // TODO: Show success message on front end
+  // TODO: Show success message on front end, or at least improve this response.
   res.send('message received');
+
+  // TODO: See list of the messages I've sent
 });
 
 app.post('/sms', function(req, res) {
